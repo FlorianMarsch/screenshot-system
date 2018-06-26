ab64708ca399d0a00ca7c7d73a0b26263113c8af
Florian Marsch <florianmarsch@Florians-MacBook-Pro.local>
2018-06-26 08:42:45 +0200

    make app return page content

--- src/api/index.js
+++ src/api/index.js
@@ -5,9 +5,27 @@ var router = express.Router();
 router.post('/screenshot', function (req, res) {
 
 
-    res.status(200).send(req.body);
 
 
+    var phantom = require("phantom");
+    var _ph, _page, _outObj;
+
+    phantom.create().then(function (ph) {
+        _ph = ph;
+        return _ph.createPage();
+    }).then(function (page) {
+        _page = page;
+        return _page.open('https://stackoverflow.com/');
+    }).then(function (status) {
+        console.log(status);
+        return _page.property('content');
+    }).then(function (content) {
+        res.status(200).send(content);
+        _page.close();
+        _ph.exit();
+    }).catch(function (e) {
+        console.log(e);
+    });
 
 
 });
